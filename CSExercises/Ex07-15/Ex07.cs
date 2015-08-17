43e77517f93fa71a02cf75d37ecb5556910114e7
SweZinEi <rose.golden318@gmail.com>
2015-08-17 10:48:21 +0800

    IhaveDone

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,15 +10,13 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            int salary, allowance,transport;
-            Console.Write("Enter Salary ");
-            salary = Convert.ToInt32(Console.ReadLine());
-            allowance = ((80/salary) * 100);
-
-            Console.WriteLine("The Salary is {0}", salary );
-            Console.WriteLine("The Allowance {0}", allowance);
-           // Console.WriteLine("The Transport {0}", allowance);
-           Console.ReadLine();
+            double salary, allowance, transport;
+            Console.Write("Pls enter your salary ");
+            salary = Convert.ToDouble(Console.ReadLine());
+            allowance = (salary * 0.1);
+            transport = (salary * 0.3);
+            Console.WriteLine("Your total income is {0} ", salary + allowance + transport);
+            Console.ReadLine();
         }
     }
 }
